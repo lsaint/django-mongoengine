e0b7e9755a46b26796c3f9674ca32e1b5073d73a
Ethan <ls4int@gmail.com>
2020-04-10 10:51:23 +0800

    _user_get_permissions

--- django_mongoengine/mongo_auth/models.py
+++ django_mongoengine/mongo_auth/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     _user_has_perm,
+    _user_get_permissions,
     _user_has_module_perms,
 )
 from django.db import models
@@ -341,6 +342,9 @@ class AbstractUser(BaseUser, document.Document):
             if hasattr(backend, "get_group_permissions"):
                 permissions.update(backend.get_group_permissions(self, obj))
         return permissions
+
+    def get_all_permissions(self, obj=None):
+        return _user_get_permissions(self, obj, "all")
 
     def has_perm(self, perm, obj=None):
         """
